191c55c40ff86b42f06de78d791cca33cf95fb54
zhichi <zhangchiemail@126.com>
2019-03-23 23:18:36 +0800

    发布课程提交

--- ShareLesson-web/src/main/java/com/pdsu/web/controller/UserController.java
+++ ShareLesson-web/src/main/java/com/pdsu/web/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.pdsu.web.controller;
 
+import com.github.pagehelper.PageHelper;
 import com.pdsu.mypojo.Result;
 import com.pdsu.pojo.User;
 import com.pdsu.service.RedisService;
@@ -7,16 +8,14 @@ import com.pdsu.service.UserService;
 import com.pdsu.utils.CodecUtil;
 import com.pdsu.utils.CookieUtils;
 import com.pdsu.utils.JsonUtils;
+import com.pdsu.web.base.BaseController;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -31,7 +30,7 @@ import java.util.UUID;
 @Controller
 @Api(tags = { "5,用户信息 相关接口" })
 @RequestMapping("/user")
-public class UserController {
+public class UserController extends BaseController {
 
     @Autowired
     private UserService userServiceImpl;
